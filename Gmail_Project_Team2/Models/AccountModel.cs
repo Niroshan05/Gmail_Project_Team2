8d3cbb3ccf9c5fb54b8f875067be9a8c007124e3
Niroshan Mageswaran <1000071559@hexaware.com>
2022-06-29 12:24:35 +0530

    initial commit

---
+++ Gmail_Project_Team2/Models/AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gmail_Project_Team2.Models
+{
+    public class AccountModel
+    {
+        public string Uesrname { get; set; }
+        public string Password { get; set; }
+    }
+}
